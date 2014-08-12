0cef7f473e38023d6746a2d4eb8546d7e0486cd2
Shevchik <Shev4ik.den@gmail.com>
2014-08-13 03:09:55 +0400

    Fix npc disable

--- src/org/jakub1221/herobrineai/nms/entity/EntityInjector.java
+++ src/org/jakub1221/herobrineai/nms/entity/EntityInjector.java
@@ -12,36 +12,33 @@ public class EntityInjector {
 
 	@SuppressWarnings("unchecked")
 	public static void inject() {
-		if (!HerobrineAI.isNPCDisabled) {
+		try {
+			Map<String, Class<?>> nameMap = null;
+			Map<Integer, Class<?>> idMap = null;
 			try {
-				Map<String, Class<?>> nameMap = null;
-				Map<Integer, Class<?>> idMap = null;
-				try {
-					final Field field1 = EntityTypes.class.getDeclaredField("e");
-					field1.setAccessible(true);
-					idMap = (Map<Integer, Class<?>>) field1.get(idMap);
-					final Field field2 = EntityTypes.class.getDeclaredField("c");
-					field2.setAccessible(true);
-					nameMap = (Map<String, Class<?>>) field2.get(nameMap);
-					nameMap.remove("Zombie");
-					nameMap.remove("Skeleton");
-					idMap.remove(54);
-					idMap.remove(51);
-					field1.set(null, idMap);
-					field2.set(null, nameMap);
-				} catch (NoSuchFieldException e) {
-					e.printStackTrace();
-				}
-				final Method a = EntityTypes.class.getDeclaredMethod("a", Class.class, String.class, Integer.TYPE);
-				a.setAccessible(true);
-				a.invoke(null, CustomZombie.class, "Zombie", 54);
-				a.invoke(null, CustomSkeleton.class, "Skeleton", 51);
-			} catch (Exception e5) {
-				e5.printStackTrace();
-				HerobrineAI.log.warning("[HerobrineAI] Custom NPCs have been disabled. (Incompatibility error!)");
+				final Field field1 = EntityTypes.class.getDeclaredField("e");
+				field1.setAccessible(true);
+				idMap = (Map<Integer, Class<?>>) field1.get(idMap);
+				final Field field2 = EntityTypes.class.getDeclaredField("c");
+				field2.setAccessible(true);
+				nameMap = (Map<String, Class<?>>) field2.get(nameMap);
+				nameMap.remove("Zombie");
+				nameMap.remove("Skeleton");
+				idMap.remove(54);
+				idMap.remove(51);
+				field1.set(null, idMap);
+				field2.set(null, nameMap);
+			} catch (NoSuchFieldException e) {
+				e.printStackTrace();
 			}
-		} else {
-
+			final Method a = EntityTypes.class.getDeclaredMethod("a", Class.class, String.class, Integer.TYPE);
+			a.setAccessible(true);
+			a.invoke(null, CustomZombie.class, "Zombie", 54);
+			a.invoke(null, CustomSkeleton.class, "Skeleton", 51);
+		} catch (Exception e5) {
+			e5.printStackTrace();
+			HerobrineAI.isNPCDisabled = true;
+			HerobrineAI.log.warning("[HerobrineAI] Custom NPCs have been disabled. (Incompatibility error!)");
 		}
 	}
 
